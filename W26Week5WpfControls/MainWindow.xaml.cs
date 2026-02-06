0e75980cc61f0c3f4f9e00bc2b13d3734ed779dd
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2026-02-06 10:26:03 -0500

    radiobutton eg

--- W26Week5WpfControls/MainWindow.xaml.cs
+++ W26Week5WpfControls/MainWindow.xaml.cs
@@ -20,5 +20,42 @@ namespace W26Week5WpfControls
         {
             InitializeComponent();
         }
+
+        private void btnGetHobbies_Click(object sender, RoutedEventArgs e)
+        {
+            string hobbies = "";
+
+            if (chkSinging.IsChecked == true)
+                hobbies += chkSinging.Content + "\n";
+
+            if (chkGardening.IsChecked == true)
+                hobbies += chkGardening.Content + "\n";
+
+            if (chkTravelling.IsChecked == true)
+                hobbies += chkTravelling.Content + "\n";
+
+            if (chkRiding.IsChecked == true)
+                hobbies += chkRiding.Content + "\n";
+
+            lblMessage.Content = hobbies;
+
+            
+        }
+
+        private void btnGetGender_Click(object sender, RoutedEventArgs e)
+        {
+            string gender = "";
+
+            if (rdoFemale.IsChecked == true)
+                gender = "Female";
+            else if (rdoMale.IsChecked == true)
+                gender = rdoMale.Content + "\n";
+            else if (rdoOther.IsChecked == true)
+                gender = rdoOther.Content + "\n";
+            else
+                gender = "Please select gender";
+
+            lblMessage.Content = gender;
+        }
     }
 }
